2a338953c0705d8fb621bf96efa4de73b425e2c0
imadafa <145224550+imadafa@users.noreply.github.com>
2023-09-17 21:06:11 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function docsclick() {
+    window.location.href = "https://create.roblox.com/docs"
+  }
+
+
+  function repoclick() {
+    window.location.href = "https://github.com/imadafa/imadafa.github.io"
+  }
+
+  function joinclick() {
+    window.location.href = "https://forms.gle/HX9jeFFBYDG2GbTJ9"
+  }
+
